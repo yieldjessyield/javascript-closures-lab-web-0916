3bdd5755b97407bc201a4ed60020151f42bf2a4c
Jess <jess.harrelson0707@gmail.com>
2016-11-06 18:54:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter += 1
+  };
+
+  function getBumps(){
+    return counter
+  };
+
+  return {
+    addBump,
+    getBumps
+  };
+
+}
+
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
